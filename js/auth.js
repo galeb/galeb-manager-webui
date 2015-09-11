3efbd265879fdea94c9f0d683d8756ff0cfcf787
Igor Fernandes <igor.fernandes@corp.globo.com>
2015-09-11 17:06:45 -0300

    Using token and function to verify logged user

--- js/auth.js
+++ js/auth.js
@@ -11,28 +11,33 @@ angular.module('auth', []).factory(
 
         authenticate : function(credentials, callback) {
 
-          var headers = credentials && credentials.username ? {
-            authorization : "Basic "
-                + btoa(credentials.username + ":"
+            var headers = credentials && credentials.username ? {
+                Authorization : "Basic "
+                    + btoa(credentials.username + ":"
                     + credentials.password)
-          } : {};
+            } : {};
 
-          $http.get(baseUrl + auth.loginPath, {
-            headers : headers
-        }).success(function(data) {
-            $window.sessionStorage.setItem('galeb', data);
-            $location.path(auth.homePath);
-            callback && callback(true);
-          }).error(function() {
-            $window.sessionStorage.removeItem('galeb');
-            $location.path(auth.loginPath);
-            callback && callback(false);
-          });
-
+            $http.get(baseUrl, {
+                headers : headers
+            }).then(function(response) {
+                var token = response.headers('x-auth-token');
+                if (token !== '') {
+                    $window.localStorage.setItem('galeb', token);
+                    $http.defaults.headers.common['X-Auth-Token'] = token;
+                    $http.defaults.headers.common.Authorization = 'Basic ';
+                } else {
+                    $window.localStorage.clear();
+                }
+                $location.path(auth.homePath);
+                callback && callback(true);
+            }, function(response) {
+                $window.localStorage.clear();
+                callback && callback(false);
+            });
         },
 
         clear : function() {
-            $window.sessionStorage.removeItem('galeb');
+            $window.localStorage.clear();
             $http.get(baseUrl + auth.logoutPath);
             $location.path(auth.loginPath);
         },
@@ -41,7 +46,16 @@ angular.module('auth', []).factory(
             auth.homePath = homePath;
             auth.loginPath = loginPath;
             auth.logoutPath = logoutPath;
-        }
+        },
+
+        isLoggedIn : function() {
+            if ($window.localStorage.getItem('galeb')) {
+                return true;
+            } else {
+                $window.localStorage.clear();
+                return false;
+            }
+        },
 
       };
 
