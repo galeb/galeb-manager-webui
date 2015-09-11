395a7394f3be756481a93d876362b6e38853029c
Igor Fernandes <igor.fernandes@corp.globo.com>
2015-09-11 17:08:49 -0300

    Change check authenticated user

--- js/navigation.js
+++ js/navigation.js
@@ -6,21 +6,17 @@ angular.module('navigation', ['auth']).controller(
         $scope.credentials = {};
 
         $scope.authenticated = function() {
-            if ($window.sessionStorage.getItem('galeb')) {
-                return true;
-            } else {
-                $location.path(auth.loginPath);
-                return false;
-            }
+            return auth.isLoggedIn();
         }
 
         $scope.login = function() {
             auth.authenticate($scope.credentials, function(authenticated) {
-                if ($scope.authenticated) {
-                    console.log("Login succeeded")
+                if (authenticated) {
+                    console.log("Login succeeded");
                     $scope.error = false;
                 } else {
-                    console.log("Login failed")
+                    console.log("Login failed");
+                    $scope.logout;
                     $scope.error = true;
                 }
             })
