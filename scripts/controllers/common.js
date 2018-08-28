fc13eccc26efed93e20e603f4ff5e1a93a23163f
Igor Fernandes <igor.fernandes@corp.globo.com>
2018-08-28 19:12:47 -0300

    Using roles from api request

--- scripts/controllers/common.js
+++ scripts/controllers/common.js
@@ -1,8 +1,4 @@
 angular.module('galebWebui')
-.controller('RolesController', function ($scope, ManagerService) {
-	$scope.manager = ManagerService;
-	$scope.manager.roles = ['ROLE_USER', 'ROLE_ADMIN'];
-})
 .controller('MethodsController', function ($scope, ManagerService) {
 	$scope.manager = ManagerService;
 	$scope.manager.methods = ['GET', 'POST', 'OPTIONS', 'PUT', 'HEAD', 'DELETE', 'CONNECT', 'TRACE', 'PATCH'];
